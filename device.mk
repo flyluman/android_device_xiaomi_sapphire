a15f90c2b0fe46ca5644eb55b70c6a605d013f5d
=Co-Authored-By: Andrew Thomas <itz63c@statixos.com>
2024-03-03 17:07:05 +0000

    topaz: Add Notch Bar Killer overlay
     * This overlay gets rid of the black bar in most apps/games
       and lets the screen fill past the notch.
    
    Change-Id: I8f2144c60f17a50060273a041a8418ad078e5fec
    Signed-off-by: ryzenforce990 <ryzenforce990@gmail.com>
    Signed-off-by: artemscine <artempemine@gmail.com>

--- device.mk
+++ device.mk
@@ -415,6 +415,7 @@ PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResTarget \
     FrameworksResTopaz \
+    NotchBarKiller \
     SettingsResTopaz \
     SettingsOverlayM7G \
     SettingsOverlayM7N \
