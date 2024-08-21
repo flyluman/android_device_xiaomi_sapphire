16edadf51df79d511c882001b12a698d74b73b40
Luman Rahman <flyluman@gmail.com>
2024-08-21 14:59:30 +0600

    PixelOS bringup

--- aosp_sapphire.mk
+++ aosp_sapphire.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sapphire/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sapphire
+PRODUCT_NAME := aosp_sapphire
 PRODUCT_DEVICE := sapphire
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
